cacd3ead4d16c20753d85dfe8e1e62c6caeaef49
Manorie Vachon <mvach854@mtroyal.ca>
2017-03-10 02:51:42 -0700

    Continuation of example

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/client/index.html');
 });
 
-//app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/client'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('chat message', function(msg) {
+	  //io.emit('chat', msg)
+	  console.log('message: ' + msg);
+  });
+  socket.on('disconnect', function(){
+	console.log('user disconnected');
+  });
 });
 
 http.listen(port, function(){
