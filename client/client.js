a908c1e481589747f4216c6b0f80230c493de571
Manorie Vachon <mvach854@mtroyal.ca>
2017-03-10 23:55:28 -0700

    User list and messages...

--- client/client.js
+++ client/client.js
@@ -1,11 +1,25 @@
 // shorthand for $(document).ready(...) 
 $(function() {
 	var socket = io();
+	
 	$('form').submit(function(){
 		socket.emit('chat message', $('#m').val());
 		$('#m').val('');
 		return false;
 	});
+	
+	socket.on('connect', function() {
+		socket.emit("new user");
+	});
+	
+	socket.on('update user list', function(list) {
+		$('#users').empty();
+		$.each(list, function(name, col) {
+			$('#users').append($('<li>').text(name));
+		});
+		console.log(list);
+	});
+	
 	socket.on('chat message', function(msg){
 		$('#messages').append($('<li>').text(msg));
 	});
--- index.js
+++ index.js
@@ -4,19 +4,36 @@ var http = require('http').Server(app); //createServer
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+let newUserID;
+let online = {};
+let chat = [];
+
 app.use(express.static(__dirname + '/client'));
 
+http.listen(port, function(){
+  console.log('listening on port', port);
+  newUserID = 0;
+});
+
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.on('new user', function() {
+	  let newID = newUserID + 1;
+	  let name = "User" + newID;
+	  online[socket.id] = {nick: name, nickColour: "black"};
+	  io.emit('update user list', online);
+ 	  console.log(online[socket.id].nick + " is now connected");
+  });
+  
   socket.on('chat message', function(msg) {
 	  io.emit('chat message', msg);
 	  console.log('message', msg);
   });
+  
   socket.on('disconnect', function(){
-	console.log('user disconnected');
+	  if (online[socket.id] != null) {
+		console.log(online[socket.id].nick + " has disconnected");
+	  }
+	  delete online[socket.id];
+	  io.emit('update user list', online);
   });
-});
-
-http.listen(port, function(){
-  console.log('listening on port', port);
 });
