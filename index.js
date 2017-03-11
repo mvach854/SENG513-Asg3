b86ac00363988bad13431eacb112681e92823cf7
Manorie Vachon <mvach854@mtroyal.ca>
2017-03-10 22:00:11 -0700

    layout to bottom

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app); //createServer
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/client/index.html');
-});
-
-//app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/client'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('chat message', function(msg) {
-	  //io.emit('chat', msg)
-	  console.log('message: ' + msg);
+	  io.emit('chat message', msg);
+	  console.log('message', msg);
   });
   socket.on('disconnect', function(){
 	console.log('user disconnected');
